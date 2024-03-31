48fbbb3d44939595850db485c3c84602aba10d15
Christian Hoffmann <chrmhoffmann@gmail.com>
2024-03-31 16:43:02 +0200

    fixup! Merge

--- drivers/media/platform/msm/camera/cam_isp/cam_isp_context.c
+++ drivers/media/platform/msm/camera/cam_isp/cam_isp_context.c
@@ -935,19 +935,6 @@ static int __cam_isp_ctx_reg_upd_in_sof(struct cam_isp_context *ctx_isp,
 			CAM_ISP_STATE_CHANGE_TRIGGER_REG_UPDATE,
 			req->request_id);
 
-	if (req_isp && req_isp->hw_update_data.fps)
-		ctx_isp->fps = req_isp->hw_update_data.fps;
-
-	if (ctx_isp->frame_id == 1)
-		ctx_isp->irq_timestamps = rup_event_data->irq_mono_boot_time;
-	else if (ctx_isp->fps && ((rup_event_data->irq_mono_boot_time -
-		ctx_isp->irq_timestamps) > ((1000*1000)/ctx_isp->fps))) {
-		ctx_isp->irq_delay_detect = true;
-		trace_cam_isp_irq_delay_detect("IRQ delay at reg_upd",
-			ctx, req ? req->request_id : 0,
-			ctx_isp->substate_activated,
-			(rup_event_data->irq_mono_boot_time -
-			ctx_isp->irq_timestamps));
 	}
 end:
 	return rc;
